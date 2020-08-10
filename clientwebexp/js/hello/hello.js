ab9a719d359cae123b2e56f4edadd0b8e4603aed
BugbearR <bugbearr@bugbearr.jp>
2020-08-10 22:41:58 +0900

    Add script element async sample

--- clientwebexp/js/hello/hello.js
+++ clientwebexp/js/hello/hello.js
@@ -5,15 +5,5 @@
         root.insertAdjacentHTML("afterbegin", "<h1>Hello, world!</h1>");
     }
 
-    if (document.readyState === "loading") {
-        window.addEventListener("DOMContentLoaded", init);
-    }
-    else if (document.readyState === "interactive"
-        || document.readyState === "complete") {
-        init();
-    }
-    else {
-        console.log("unknown document.readyState:" + document.readyState);
-    }
+    window.addEventListener("DOMContentLoaded", init);
 })();
-
---
+++ clientwebexp/js/script_element/async/hello.js
@@ -0,0 +1,18 @@
+(function () {
+    function init() {
+        document.title = "Hello, world!";
+        var root = document.getElementById("root");
+        root.insertAdjacentHTML("afterbegin", "<h1>Hello, world!</h1>");
+    }
+
+    if (document.readyState === "loading") {
+        window.addEventListener("DOMContentLoaded", init);
+    }
+    else if (document.readyState === "interactive"
+        || document.readyState === "complete") {
+        init();
+    }
+    else {
+        console.log("unknown document.readyState:" + document.readyState);
+    }
+})();
