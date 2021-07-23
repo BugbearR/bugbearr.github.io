413c96d60cf9970d22fdfba6a65cff48895a9672
BugbearR <bugbearr@bugbearr.jp>
2021-07-23 13:47:53 +0900

    Add surrogate pair compare

--- clientwebexp/browser_capability_test/UTF-32_literal/index.js
+++ clientwebexp/browser_capability_test/UTF-32_literal/index.js
@@ -1,6 +1,11 @@
 (function () {
     function init() {
-        document.forms[0].result.value = "\u{0004f}\u{0004b}";
+        if ("\u{0004f}\u{0004b}" === "OK"
+                && "\u{2000B}" === "\uD840\uDC0B") {
+            document.forms[0].result.value = "\u{0004f}\u{0004b}";
+        } else {
+            document.forms[0].result.value = "NG";
+        }
     }
 
     window.onload = function () { init(); };
