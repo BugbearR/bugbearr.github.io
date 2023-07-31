7d28fab593a0285be2c7a4973a91fc522d39ed6e
BugbearR <bugbearr@bugbearr.jp>
2023-07-31 13:37:03 +0900

    Add viewport, show width, height

--- clientwebexp/css/height100per/sample2/index.js
+++ clientwebexp/css/height100per/sample2/index.js
@@ -1,4 +1,28 @@
 (function () {
+    function showScreenSize() {
+        document.getElementById("screenWidth").textContent = window.screen.width;
+        document.getElementById("screenHeight").textContent = window.screen.height;
+        document.getElementById("screenOrientation").textContent = `type: ${window.screen.orientation.type} angle: ${window.screen.orientation.angle}`;
+    }
+
+    function showWindowSize() {
+        document.getElementById("innerWidth").textContent = window.innerWidth;
+        document.getElementById("innerHeight").textContent = window.innerHeight;
+    }
+
+    function showDivRootSize() {
+        const divRoot = document.getElementById("root");
+        const divRootStyle = window.getComputedStyle(divRoot);
+        document.getElementById("divRootWidth").textContent = divRootStyle.width;
+        document.getElementById("divRootHeight").textContent = divRootStyle.height;
+    }
+
+    function showSizeAll() {
+        showScreenSize();
+        showWindowSize();
+        showDivRootSize();
+    }
+
     function init() {
         document.getElementById("ua").appendChild(document.createTextNode(navigator.userAgent));
 
@@ -10,6 +34,13 @@
         for (const prop of props) {
             console.log(`${prop}: ${style.getPropertyValue(prop)}`);
         }
+
+        window.addEventListener("resize", function () {
+            showSizeAll();
+        });
+        window.addEventListener("orientationchange", function () {
+            showSizeAll();
+        });
     }
 
     init();
