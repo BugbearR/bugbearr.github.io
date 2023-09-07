163ede85588849c5d41a0a2a211f44d9308b11d1
BugbearR <bugbearr@bugbearr.jp>
2023-09-07 17:11:02 +0900

    Add pointer_event_multi

---
+++ clientwebexp/js/pointer_device/pointer_event_multi/index.js
@@ -0,0 +1,46 @@
+(function () {
+    var pointerIdMap = new Map();
+    function init() {
+        var rootElm = document.getElementById("root");
+        new ResizeObserver(() => {
+            var svgElm = document.getElementById("svg1");
+            var rootRect = rootElm.getBoundingClientRect();
+            svgElm.setAttribute("viewBox", `${rootRect.x} ${rootRect.y} ${rootRect.width} ${rootRect.height}`);
+            svgElm.setAttribute("width", `${rootRect.width}px`);
+            svgElm.setAttribute("height", `${rootRect.height}px`);
+        }).observe(rootElm);
+
+        var svgElm = document.getElementById("svg1");
+        svgElm.addEventListener("pointermove", (evt) => {
+            let pointerNo = pointerIdMap.get(evt.pointerId);
+            if (pointerNo === undefined) {
+                pointerNo = pointerIdMap.size;
+                pointerIdMap.set(evt.pointerId, pointerNo);
+            }
+            // pressureに基づいてcircleの半径を計算
+            let radius = evt.pressure * 5;
+
+            // circle要素を作成
+            let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+            circle.setAttribute("cx", evt.clientX);
+            circle.setAttribute("cy", evt.clientY);
+            if (radius === 0) {
+                radius = 0.75;
+            }
+            circle.setAttribute("r", radius);
+            if (pointerNo >= 10) {
+                circle.setAttribute("fill", "brack");
+            } else {
+                circle.classList.add(`code${pointerNo}`);
+            }
+            svgElm.appendChild(circle);
+
+            // 3秒後にcircleを削除
+            setTimeout(() => {
+                svgElm.removeChild(circle);
+            }, 3000);
+        });
+    }
+
+    window.addEventListener("DOMContentLoaded", init);
+})();
