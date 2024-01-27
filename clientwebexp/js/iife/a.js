4c49a5acd9658107d2a799da1b104d96a7106bc6
BugbearR <bugbearr@bugbearr.jp>
2024-01-27 12:58:56 +0900

    Add IIFE (Immediately Invoked Function Expression) files and update index.html

---
+++ clientwebexp/js/iife/a.js
@@ -0,0 +1,10 @@
+(function () {
+    var x = 1;
+    function showX() {
+        alert("x is " + x + " in a.js");
+    }
+    // window.onload = function () {
+    window.addEventListener("load", function () {
+        showX();
+    });
+})();
