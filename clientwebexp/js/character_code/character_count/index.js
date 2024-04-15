fde94988dee14e0eff4d269da40ef517bf90d837
BugbearR <bugbearr@bugbearr.jp>
2024-04-15 14:59:03 +0900

    Refactor character code conversion logic to handle empty input values

--- clientwebexp/js/character_code/character_count/index.js
+++ clientwebexp/js/character_code/character_count/index.js
@@ -23,7 +23,7 @@
             for (let i = 0; i < value.length; i++) {
                 utf16List.push(value.charCodeAt(i));
             }
-            const strUtf16 = "['" + utf16List.map((code)=> { return "\\u" + code.toString(16).padStart(4, "0"); }).join("','") + "']";
+            const strUtf16 = (utf16List.length > 0) ? "['" + utf16List.map((code)=> { return "\\u" + code.toString(16).padStart(4, "0"); }).join("','") + "']" : "[]";
             document.getElementById("outputUtf16").textContent = strUtf16;
 
             const utf32List = [];
@@ -35,7 +35,7 @@
                 }
             }
             document.getElementById("outputUtf32Length").textContent = utf32List.length;
-            const strUtf32 = "['" + utf32List.map((code)=> { return "\\U" + code.toString(16).padStart(6, "0"); }).join("','") + "']";
+            const strUtf32 = (utf32List.length > 0) ? "['" + utf32List.map((code)=> { return "\\U" + code.toString(16).padStart(6, "0"); }).join("','") + "']" : "[]";
             document.getElementById("outputUtf32").textContent = strUtf32;
 
             const utf8Blob = new Blob([value], {type: "text/plain;charset=utf-8"});
@@ -44,7 +44,7 @@
 
             const utf8Buffer = await utf8Blob.arrayBuffer();
             const uint8Array = new Uint8Array(utf8Buffer);
-            const strUtf8 = "['" + Array.from(uint8Array).map((code)=> { return code.toString(16).padStart(2, "0"); }).join("','") + "']";
+            const strUtf8 = (utf8Count > 0) ? "['" + Array.from(uint8Array).map((code)=> { return code.toString(16).padStart(2, "0"); }).join("','") + "']" : "[]";
             document.getElementById("outputUtf8").textContent = strUtf8;
 
             const locale = document.getElementById("inputLocale").value;
@@ -53,7 +53,7 @@
             const segments = Array.from(segmenter.segment(value));
             const graphemeCount = Array.from(segments).length;
             document.getElementById("outputGraphemeLength").textContent = graphemeCount;
-            const strGrapheme = "['" + segments.map(item => item.segment).join("','") + "']";
+            const strGrapheme = (graphemeCount > 0) ? "['" + segments.map(item => item.segment).join("','") + "']" : "[]";
             document.getElementById("outputGrapheme").textContent = strGrapheme;
         });
     }
