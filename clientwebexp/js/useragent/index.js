f8a6516fa40488ea9f4aaae2fd59649cfddb3dac
BugbearR <bugbearr@bugbearr.jp>
2024-09-15 01:11:04 +0000

    Implove useragent displaying

--- clientwebexp/js/useragent/index.js
+++ clientwebexp/js/useragent/index.js
@@ -1,7 +1,3 @@
-(function () {
-    function init() {
-        document.forms[0].useragent.value = navigator.userAgent;
-    }
-
-    init();
-})();
+window.onload = function() {
+    document.forms[0].useragent.value = navigator.userAgent;
+}
